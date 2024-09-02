684097d90d9bba86bc64cf4fd7dee290e2dd5c87
sol5288 <solosols@gmail.com>
2024-09-02 14:36:25 +0900

    Delete mysql-init.sql

--- mysql-init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `one-api`;
-USE `one-api`;
-
-CREATE TABLE IF NOT EXISTS `plugin` (
-    `name` varchar(64) NOT NULL DEFAULT '',
-    `dl` varchar(128) NOT NULL DEFAULT '',
-    PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
